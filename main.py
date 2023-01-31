4f05b35df88ec24a9156b1fdc63e5e55ac1c2e05
Jamie <jamiesiennagold@gmail.com>
2023-01-31 22:00:47 +1100

    finished main()

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ from time import sleep
 from i2c_lcd1602 import Screen
 from gpiozero import Button, RotaryEncoder
 from signal import pause
+from stopwatch import Stopwatch
+from datetime import timedelta
+from math import floor
 
 '''
-To run the program, the necessary dependancies are gpiozero, RPi.GPIO, stopwatch.py
+To run the program, the necessary dependancies are gpiozero, RPi.GPIO, ez-stopwatch
 RPi.GPIO is preinstalled
-sudo pip install stopwatch.py
+sudo pip3 install ez-stopwatch
 sudo pip3 install gpiozero
 '''
 
@@ -15,12 +18,20 @@ screen = Screen(bus=1, addr=0x3f, cols=16, rows=2)
 button = Button(4, bounce_time=1)
 rot_but = Button(22)
 rotary = RotaryEncoder(17, 27, max_steps=0)
+turn_timer = Stopwatch()
+game_timer = Stopwatch()
 
 turn_phases = ['Untap', 'Upkeep', 'Draw', 'Main Phase 1', 'Begin Combat', 'Declare Attacker',
                'Declare Blockers', 'Combat Damage', 'End Combat', 'Main Phase 2', 'End Phase']
 
 left = False
 right = False
+
+
+def dict_key_name(dict_name: str, key_of_dict: int):
+
+    key_name = list(dict_name.keys())[key_of_dict]
+    return key_name
 
 
 def turn_left():
@@ -113,10 +124,19 @@ def main():
     life_editor = False
     player_life_editor = 0
 
+    stats_menu = 0
+    action_menu = 0
+    actions = ["Forfeit?", "Restart?"]
+
+    game_timer.start()
+    turn_timer.start()
+
     while game == True:
 
         rotary.when_rotated_clockwise = turn_right
         rotary.when_rotated_counter_clockwise = turn_left
+        statistic_list = [f"Turn: {turns}", f"TurnTime {timedelta(seconds=floor(turn_timer.time_elapsed))}",
+                          f"GameTime {timedelta(seconds=floor(game_timer.time_elapsed))}"]
 
         if menu == 0:
             '''
@@ -129,15 +149,21 @@ def main():
                 player_turn = 0
 
             if rot_but.is_pressed:
+                turn_timer.stop()
+                turn_timer.reset()
                 current_phase = 0
                 player_turn = player_turn + 1
                 turns = turns + 1
                 sleep(0.5)
+                turn_timer.start()
 
             if current_phase == len(turn_phases):
+                turn_timer.stop()
+                turn_timer.reset()
                 current_phase = 0
                 player_turn = player_turn + 1
                 turns = turns + 1
+                turn_timer.start()
 
             if right == True:
                 current_phase = current_phase + 1
@@ -154,44 +180,116 @@ def main():
             screen.display_data(turn_phases[current_phase % len(turn_phases)])
 
         elif menu == 1:
-            if rot_but.is_pressed:
-                life_editor = True
-            
             if life_editor == False:
-                if right == True:
-                    if player_life_editor < (len(player_life) - 1):
-                        player_life_editor = player_life_editor + 1
-                        right = False
+                if rot_but.is_pressed:
+                    life_editor = True
+                    sleep(0.2)
 
-                if left == True:
-                    if player_life_editor > 0:
-                        player_life_editor = player_life_editor - 1
-                        left = False
+                if (right == True) and (player_life_editor < (len(player_life) - 1)):
+                    player_life_editor = player_life_editor + 1
+                    right = False
+                else:
+                    right = False
+
+                if (left == True) and (player_life_editor > 0):
+                    player_life_editor = player_life_editor - 1
+                    left = False
+                else:
+                    left = False
+
                 screen.cursorTo(0, 1)
                 screen.display_data("Edit Player Life")
                 screen.cursorTo(1, 1)
-                screen.display_data(f"{list(player_life.keys())[player_life_editor]}")
+                screen.display_data(
+                    f"{dict_key_name(player_life, player_life_editor)}")
             else:
+                if rot_but.is_pressed:
+                    life_editor = False
+                    sleep(0.2)
+
+                if left == True:
+                    player_life[dict_key_name(player_life, player_life_editor)] = player_life[dict_key_name(
+                        player_life, player_life_editor)] - 1
+                    left = False
+
+                if right == True:
+                    player_life[dict_key_name(player_life, player_life_editor)] = player_life[dict_key_name(
+                        player_life, player_life_editor)] + 1
+                    right = False
+
                 screen.cursorTo(0, 1)
-                screen.display_data(f"{list(player_life.keys())[player_life_editor]}'s Life")
+                screen.display_data(
+                    f"{dict_key_name(player_life, player_life_editor)}'s Life")
                 screen.cursorTo(1, 1)
-                screen.display_data(str(player_life[player_life_editor]))
+                screen.display_data(
+                    str(player_life[dict_key_name(player_life, player_life_editor)]))
 
         elif menu == 2:
             screen.cursorTo(0, 1)
             screen.display_data("Game Statistics")
+
+            if (right == True) and (stats_menu < 2):
+                stats_menu = stats_menu + 1
+                right = False
+            else:
+                right = False
+
+            if (left == True) and (stats_menu > 0):
+                stats_menu = stats_menu - 1
+                left = False
+            else:
+                left = False
+
             screen.cursorTo(1, 1)
-            screen.display_data(f"Turn {turns} Time {}")
+            screen.display_data(statistic_list[stats_menu])
+
+        elif menu == 3:
+            screen.cursorTo(0, 1)
+            screen.display_data("Choose Action")
+
+            if (right == True) and (action_menu < 1):
+                action_menu = action_menu + 1
+                right = False
+            else:
+                right = False
+
+            if (left == True) and (action_menu > 0):
+                action_menu = action_menu - 1
+                left = False
+            else:
+                left = False
+
+            screen.cursorTo(1, 1)
+            screen.display_data(actions[action_menu])
+
+            if (rot_but.is_pressed) and (action_menu == 0):
+                game = False
+                ending = "Forfeit"
+            elif (rot_but.is_pressed) and (action_menu == 1):
+                restart()
 
         else:
             menu = 0
 
         if button.is_pressed == True:
             menu = menu + 1
-            sleep(0.5)
-        for x, life in player_life.items():
+            sleep(0.2)
+
+        player_life_copy = player_life.copy()
+
+        for players_left, life in player_life_copy.items():
             if life == 0:
-                game == False
+                screen.cursorTo(0, 1)
+                screen.display_data(f"  {players_left}  ")
+                screen.cursorTo(1, 1)
+                screen.display_data("   Eliminated   ")
+                player_life.pop(players_left)
+                life_editor = False
+                sleep(2)
+
+        if len(player_life) == 1:
+            ending = f"{dict_key_name(player_life, 0)} Won"
+            game = False
 
 
 def end():
